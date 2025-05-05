8366ac3642057c416540602e07d9ab3f5fac6fa4
Cristian Tiut <tiutcristian@gmail.com>
2025-05-06 00:10:27 +0300

    Refactor Main page

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,18 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
-import { Listing, LocalStorageAction } from './types';
+import { Listing, LocalStorageAction, UserType } from './types';
 import ListingCard from '@/components/ListingCard';
 import { addActionToQueue, deleteLocalListing, getLocalListings, setLocalListings } from './offlineSupport/CRUDLocalStorage';
 import { isServerUp } from './apiCalls/serverStatus';
 import { Spinner } from '@/components/Spinner';
-import { fetchFilteredListings, fetchListings, getNoOfPages, getNoOfPagesFiltered } from './apiCalls/fetchListings';
+import { fetchFilteredListings } from './apiCalls/fetchListings';
 import { deleteListing } from './apiCalls/deleteListing';
 import { useInView } from 'react-intersection-observer';
 import { delay } from '@/lib/utils';
 import ButtonGroup from '@/components/ButtonGroup';
 import SearchBar from '@/components/SearchBar';
+import { fetchUsersAsync } from './apiCalls/fetchUsers';
 
 
 export default function Home() {
@@ -23,62 +24,67 @@ export default function Home() {
 	const [loading, setLoading] = useState(true);
 	const [pagesLoaded, setPagesLoaded] = useState(0);
 	const [totalPages, setTotalPages] = useState(0);
+	const [usersList, setUsersList] = useState<UserType[]>([]);
+	const [currentUser, setCurrentUser] = useState<UserType>();
     const { ref, inView } = useInView();
 
-	const fetchMoreListings = async () => {
-        await delay(1000);
-        const nextPage = pagesLoaded + 1;
-        var newListings: Listing[] | null = null;
+	const fetchListings = async (page: number, size: number) => {
+		const params = new URLSearchParams({
+			'userId': currentUser?.id?.toString() || '1',
+			'page': page.toString(),
+			'size': size.toString()
+		});
 		if (search !== '') {
-			const params = new URLSearchParams({
-				'page': nextPage.toString(),
-				'size': '5',
-				'title': search,
-			});
-			newListings = await fetchFilteredListings(params);
-		} else {
-			newListings = await fetchListings(nextPage, 5);
+			params.set('title', search);
 		}
+		const fetchedData = await fetchFilteredListings(params);
+		return fetchedData;
+	};
+
+	const fetchData = async () => {
+		if (await isServerUp()) {
+			const fetchedData = await fetchListings(0, 5);
+			setListings(fetchedData?.data || []);
+			setLocalListings(fetchedData?.data || []);
+			setTotalPages(fetchedData?.totalPages || 0);
+			setPagesLoaded(0);
+			setLoading(false);
+		} else {
+			const localListings = getLocalListings();
+			setListings(localListings);
+			setLoading(false);
+		}
+	};
+
+	const fetchMoreListings = async (size: number) => {
+        await delay(500);
+        const nextPage = pagesLoaded + 1;
+		var newListings = await fetchListings(nextPage, size);
 		console.log('Fetched page:', nextPage);
-        setListings((prevListings) => [...prevListings, ...(newListings || [])]);
+        setListings((prevListings) => [...prevListings, ...(newListings?.data || [])]);
         setPagesLoaded(nextPage);
     };
 
-    useEffect(() => {
+	useEffect(() => {
         if (inView && pagesLoaded < totalPages - 1) {
             console.log("Loading more listings...");
-            fetchMoreListings();
+            fetchMoreListings(5);
         }
     }, [inView]);
 
 	useEffect(() => {
-		const fetchNoOfPages = async () => {
-			if (await isServerUp()) {
-				const noOfPages = await getNoOfPages(5);
-				if (noOfPages) {
-					setTotalPages(noOfPages);
-					console.log('Total pages:', noOfPages);
-				}
-			}
-		};
-		fetchNoOfPages();
-	}, []);
+		fetchData();
+	}, [currentUser]);
 
 	useEffect(() => {
-		const fetchData = async () => {
+		const fetchUsers = async () => {
 			if (await isServerUp()) {
-				const fetchedListings = await fetchListings(0, 5);
-				setListings(fetchedListings || []);
-				setLocalListings(fetchedListings || []);
-				setLoading(false);
-			} else {
-				const localListings = getLocalListings();
-				setListings(localListings);
-				setLoading(false);
+				const fetchedUsers = await fetchUsersAsync();
+				setUsersList(fetchedUsers || []);
+				setCurrentUser(fetchedUsers ? fetchedUsers[0] : undefined);
 			}
 		};
-
-		fetchData();
+		fetchUsers();
 	}, []);
 
 	const handleDelete = async (id: number) => {
@@ -94,41 +100,6 @@ export default function Home() {
 			}
 		}
 	};
-
-	const handleSearch = async (search: string) => {
-		setLoading(true);
-
-		if (await isServerUp()) {
-			const params = new URLSearchParams({
-				'page': '0',
-				'size': '5',
-				'title': search,
-			});
-			const fetchedListings = await fetchFilteredListings(params);
-			if (fetchedListings) {
-				setListings(fetchedListings);
-				setLocalListings(fetchedListings);
-			} else {
-				console.error('Error fetching filtered listings');
-			}
-		} else {
-			const localListings = getLocalListings();
-			const filteredListings = localListings.filter((listing: Listing) =>
-				listing.title.toLowerCase().includes(search.toLowerCase())
-			);
-			setListings(filteredListings);
-		}
-
-		setPagesLoaded(0);
-		const noOfPages = await getNoOfPagesFiltered(new URLSearchParams({
-			'page': '0',
-			'size': '5',
-			'title': search,
-		}));
-		setTotalPages(noOfPages || 0);
-
-		setLoading(false);
-	}
 		
 	if (loading) {
 		return (
@@ -146,16 +117,14 @@ export default function Home() {
 	return (
 		<main className="p-8 pb-20 sm:p-20 font-sans flex flex-col items-center gap-8">
 			<ButtonGroup
-				children={["User1", "User2", "User3", "User4", "User5"]}
-				onClick={(index) => {
-					alert(`Button ${index + 1} clicked`);
-				}}
+				children={usersList.map((user) => (user.firstName + ' ' + user.lastName))}
+				onClick={index => setCurrentUser(usersList[index])}
 			/>
 			
 			<SearchBar 
 				search={search}
 				setSearch={setSearch}
-				handleSearch={handleSearch}
+				handleSearch={fetchData}
 			/>
 
 			<Link 
