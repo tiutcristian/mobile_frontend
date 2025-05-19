804eade5b790271811f9825a0e6a37c54283e892
Cristian Tiut <tiutcristian@gmail.com>
2025-05-19 21:17:22 +0300

    update utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,11 +3,9 @@ export function delay(ms: number) {
 }
 
 export function getBaseUrl() {
-    console.log('process.env.BACKEND_PRIVATE_DOMAIN', process.env.BACKEND_PRIVATE_DOMAIN)
     return process.env.BACKEND_PRIVATE_DOMAIN
 }
 
 export function getAPIKey() {
-    console.log('process.env.API_KEY', process.env.API_KEY)
-    return process.env.API_KEY
+    return process.env.API_KEY || ""
 }
