50c96d1d302f8cc7e6fdca41df46ed18749e5aa7
Cristian Tiut <tiutcristian@gmail.com>
2025-05-29 12:34:23 +0300

    Create insert_listings_sql_script.sql

---
+++ app/insert_listings_sql_script.sql
@@ -0,0 +1,67 @@
+INSERT INTO listings (user_id, image_url, title, price, make, model, description, manufacture_year, mileage, engine_size, horsepower, transmission, fuel_type)
+VALUES (
+    1,
+    '/images/corolla.png',
+    'Toyota Corolla 2020 for sale',
+    20000.0,
+    'Toyota',
+    'Corolla',
+    'For Sale: 2020 Toyota Corolla - Reliable & Fuel Efficient!',
+    '2023',
+    10000,
+    1800,
+    140,
+    'AUTOMATIC',
+    'DIESEL'
+);
+
+INSERT INTO listings (user_id, image_url, title, price, make, model, description, manufacture_year, mileage, engine_size, horsepower, transmission, fuel_type)
+VALUES (
+    1,
+    '/images/yaris.png',
+    'Toyota Yaris 2019 for sale',
+    15000.0,
+    'Toyota',
+    'Yaris',
+    'This is a 2019 Toyota Yaris for sale',
+    '2019',
+    20000,
+    1500,
+    110,
+    'MANUAL',
+    'PETROL'
+);
+
+INSERT INTO listings (user_id, image_url, title, price, make, model, description, manufacture_year, mileage, engine_size, horsepower, transmission, fuel_type)
+VALUES (
+    1,
+    '/images/camry.png',
+    'Toyota Camry 2018 for sale',
+    25000.0,
+    'Toyota',
+    'Camry',
+    'This is a 2018 Toyota Camry for sale',
+    '2018',
+    30000,
+    2500,
+    200,
+    'AUTOMATIC',
+    'HYBRID'
+);
+
+INSERT INTO listings (user_id, image_url, title, price, make, model, description, manufacture_year, mileage, engine_size, horsepower, transmission, fuel_type)
+VALUES (
+    1,
+    '/images/rav4.png',
+    'Toyota RAV4 2017 for sale',
+    30000.0,
+    'Toyota',
+    'RAV4',
+    'This is a 2017 Toyota RAV4 for sale',
+    '2000',
+    40000,
+    2000,
+    150,
+    'AUTOMATIC',
+    'DIESEL'
+);
